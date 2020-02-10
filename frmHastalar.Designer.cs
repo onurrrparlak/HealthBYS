51dee6d2059c33e8395ab13fdd17be1201a4338c
onurrrparlak <onurrrparlak@gmail.com>
2020-02-10 16:22:46 +0300

    HastaListeleme

---
+++ frmHastalar.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace HealthBYS
+{
+    partial class frmHastalar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnYeniHastaKayit = new System.Windows.Forms.Button();
+            this.btnDuzenle = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvHastalar = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(649, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ana Menü";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnYeniHastaKayit
+            // 
+            this.btnYeniHastaKayit.Location = new System.Drawing.Point(12, 12);
+            this.btnYeniHastaKayit.Name = "btnYeniHastaKayit";
+            this.btnYeniHastaKayit.Size = new System.Drawing.Size(98, 32);
+            this.btnYeniHastaKayit.TabIndex = 1;
+            this.btnYeniHastaKayit.Text = "Yeni Kayıt";
+            this.btnYeniHastaKayit.UseVisualStyleBackColor = true;
+            this.btnYeniHastaKayit.Click += new System.EventHandler(this.btnYeniHastaKayit_Click);
+            // 
+            // btnDuzenle
+            // 
+            this.btnDuzenle.Location = new System.Drawing.Point(127, 12);
+            this.btnDuzenle.Name = "btnDuzenle";
+            this.btnDuzenle.Size = new System.Drawing.Size(98, 32);
+            this.btnDuzenle.TabIndex = 2;
+            this.btnDuzenle.Text = "Düzenle";
+            this.btnDuzenle.UseVisualStyleBackColor = true;
+            this.btnDuzenle.Click += new System.EventHandler(this.btnDuzenle_click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(241, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 32);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(359, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(98, 32);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Aktar";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 100);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Hasta Telefon No:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hasta TC Kimlik:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hasta No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasta Ad ve Soyadı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arama";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lvHastalar
+            // 
+            this.lvHastalar.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvHastalar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lvHastalar.FullRowSelect = true;
+            this.lvHastalar.GridLines = true;
+            this.lvHastalar.HideSelection = false;
+            this.lvHastalar.Location = new System.Drawing.Point(12, 157);
+            this.lvHastalar.Name = "lvHastalar";
+            this.lvHastalar.Size = new System.Drawing.Size(776, 281);
+            this.lvHastalar.TabIndex = 6;
+            this.lvHastalar.UseCompatibleStateImageBehavior = false;
+            this.lvHastalar.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Hasta No";
+            this.columnHeader1.Width = 98;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Hasta TC";
+            this.columnHeader2.Width = 132;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Adı";
+            this.columnHeader3.Width = 79;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Soyadı";
+            this.columnHeader4.Width = 71;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Doğum Tarihi";
+            this.columnHeader5.Width = 139;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Telefon No";
+            this.columnHeader6.Width = 125;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Hasta Borcu";
+            this.columnHeader7.Width = 134;
+            // 
+            // frmHastalar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 484);
+            this.Controls.Add(this.lvHastalar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnDuzenle);
+            this.Controls.Add(this.btnYeniHastaKayit);
+            this.Controls.Add(this.button1);
+            this.Name = "frmHastalar";
+            this.Text = "frmHastalar";
+            this.Load += new System.EventHandler(this.frmHastalar_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnYeniHastaKayit;
+        private System.Windows.Forms.Button btnDuzenle;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView lvHastalar;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
---
+++ frmHastalar.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthBYS
+{
+    public partial class frmHastalar : Form
+    {
+        public frmHastalar()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmMenu menu = new frmMenu();
+            menu.Show();
+        }
+
+        private void btnYeniHastaKayit_Click(object sender, EventArgs e)
+        {
+          
+            frmHastaEkleme m = new frmHastaEkleme();
+            cGenel._hastaEkleme = 1;
+            m.Show();
+        }
+
+        private void frmHastalar_Load(object sender, EventArgs e)
+        {
+            cHastalar c = new cHastalar();
+            c.hastalariGetir(lvHastalar);
+            
+        }
+
+        private void btnDuzenle_click(object sender, EventArgs e)
+        {
+            if (lvHastalar.SelectedItems.Count > 0)
+            {
+                frmHastaEkleme frm = new frmHastaEkleme();
+                cGenel._hastaEkleme = 1;
+                cGenel._hastaID = Convert.ToInt32(lvHastalar.SelectedItems[0].SubItems[0].Text);
+
+                
+                
+                frm.Show();
+            }
+        }
+    }
+}
